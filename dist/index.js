09b01b99a525cf9627790ac1cdedd531c6f730d0
shubhasispadhi11031998 <shubhasis.padhi@successive.tech>
2020-11-04 18:53:03 +0530

    Mongoose and promises

--- dist/index.js
+++ dist/index.js
@@ -3,6 +3,6 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const server_1 = require("./server");
 const configuration_1 = require("./config/configuration");
 console.log('config is', configuration_1.default);
-const server = new server_1.default({ PORT: 9000 });
+const server = new server_1.default(configuration_1.default);
 server.bootstrap().run();
 //# sourceMappingURL=index.js.map
--- src/config/IConfig.ts
+++ src/config/IConfig.ts
@@ -1,5 +1,6 @@
 interface IConfig {
     PORT: number;
     NODE_ENV: string;
+    MONGO_URL: string;
 }
 export { IConfig };
